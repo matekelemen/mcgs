400b7c144f46fe9547e85989df38fb55fe692dcc
matekelemen <kelemen.mate.12@gmail.com>
2024-08-04 12:08:12 +0200

    disable asan

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ if ! cmake                                      \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON          \
     -DMCGS_BUILD_TESTS=ON                       \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo           \
-    -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -fsanitize=address"
+    -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer"
     then exit 1
 fi
 
