7b06a8672376f22e24cd8ec6ad177ab885c77c5d
matekelemen <kelemen.mate.12@gmail.com>
2024-08-07 00:17:38 +0200

    add macos testrunner

---
+++ test/testrunner.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Set working directory to the <repo-root>/test.
+scriptDir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$scriptDir"
+
+# Check if test files are available and extract them if they're not.
+for fileName in mok_structure.mm mok_structure_rhs.mm mok_fluid.mm mok_fluid_rhs.mm; do
+    if ! [ -f $fileName ]; then
+        if ! tar -xvzf input.tar.gz; then
+            exit 1
+        fi
+        break
+    fi
+done
+
+# Run tests
+cd ..
+for shrinkingFactor in 1 8 64 256; do
+    for threadCount in 1 2 4; do
+        for case in mok_structure mok_fluid; do
+            echo "Running $case on $threadCount threads with $shrinkingFactor shrinking ..."
+            export OMP_NUM_THREADS=$threadCount
+            if ! build/bin/mcgs_testrunner                \
+                    test/${case}.mm test/${case}_rhs.mm   \
+                    -s $shrinkingFactor                   \
+                    -i 1e2
+                then exit 1
+            fi
+        done
+    done
+done
