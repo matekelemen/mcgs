934533e971b1bfd702c9fb54ef3e3b60f5fa135b
matekelemen <kelemen.mate.12@gmail.com>
2024-08-07 11:45:41 +0200

    fix readme for doxygen

---
+++ docs/prepareDocs.py
@@ -0,0 +1,40 @@
+# The main readme.md at the repository's root is used as the landing
+# page of the doxygen documentation, which becomes an issue when
+# resolving links (github vs local docs). This script creates a
+# separate readme.md for doxygen and replaces the links to point to
+# local or github links, depending in the provided arguments.
+
+# --- STD Imports ---
+import pathlib
+import re
+import argparse
+
+
+parser = argparse.ArgumentParser("prepareDocs")
+parser.add_argument("-w",
+                    dest = "web",
+                    action = "store_const",
+                    const = True,
+                    default = False,
+                    help = "prepare docs for the web")
+arguments = parser.parse_args()
+
+rootPath = "https://matekelemen.github.io/mcgs/" if arguments.web else "../../"
+docPath = rootPath + "docs/html/"
+assetPath = rootPath + ".github/assets/"
+
+docLinkPattern = re.compile(r"\[mcgs::(\w+)\]\(([\w0-9\.#]+)\)")
+docLinkReplace = r"[mcgs::\1](" + docPath + r"\2)"
+assetLinkPattern = re.compile(r"\.github/assets/")
+assetLinkReplace = assetPath
+
+readme = ""
+with open(pathlib.Path(__file__).absolute().parent.parent / "readme.md", "r") as inputFile:
+    readme = inputFile.read()
+
+with open(pathlib.Path(__file__).parent / "readme.md", "w") as outputFile:
+    outputFile.write(re.sub(assetLinkPattern,
+                            assetLinkReplace,
+                            re.sub(docLinkPattern,
+                                   docLinkReplace,
+                                   readme)))
